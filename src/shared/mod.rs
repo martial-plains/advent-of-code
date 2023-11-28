e6a3c4c453019c4c091b9db4eb454189f037d938
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-11-28 07:11:22 -0400

    complete day 4

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -1,2 +1,3 @@
 pub mod hash;
 pub mod point;
+pub mod md5;
---
+++ src/year_2015/day_04/mod.rs
@@ -0,0 +1,115 @@
+use crate::shared::md5::*;
+
+use std::{
+    sync::atomic::{AtomicBool, AtomicU32, Ordering},
+    thread,
+};
+
+use rayon::prelude::*;
+
+pub struct Day04 {
+    prefix: String,
+    done: AtomicBool,
+    counter: AtomicU32,
+    first: AtomicU32,
+    second: AtomicU32,
+}
+
+impl Day04 {
+    const TITLE: &'static str = "The Ideal Stocking Stuffer";
+
+    pub fn part1(input: &str) -> u32 {
+        let shared = parse(input);
+        shared.first.load(Ordering::Relaxed)
+    }
+
+    pub fn part2(input: &str) -> u32 {
+        let shared = parse(input);
+        shared.second.load(Ordering::Relaxed)
+    }
+}
+
+fn parse(input: &str) -> Day04 {
+    let shared = Day04 {
+        prefix: input.trim().to_string(),
+        done: AtomicBool::new(false),
+        counter: AtomicU32::new(1000),
+        first: AtomicU32::new(u32::MAX),
+        second: AtomicU32::new(u32::MAX),
+    };
+
+    // Handle the first 999 numbers specially as the number of digits varies.
+    (1..1000).into_par_iter().for_each(|n| {
+        let (mut buffer, size) = format_string(&shared.prefix, n);
+        check_hash(&mut buffer, size, n, &shared);
+    });
+
+    // Use as many cores as possible to parallelize the remaining search.
+    rayon::scope(|scope| {
+        for _ in 0..rayon::current_num_threads() {
+            scope.spawn(|_| worker(&shared));
+        }
+    });
+
+    shared
+}
+
+fn format_string(prefix: &str, n: u32) -> ([u8; 64], usize) {
+    let string = format!("{prefix}{n}");
+    let size = string.len();
+
+    let mut buffer = [0; 64];
+    buffer[0..size].copy_from_slice(string.as_bytes());
+
+    (buffer, size)
+}
+
+fn check_hash(buffer: &mut [u8], size: usize, n: u32, shared: &Day04) {
+    let (result, ..) = hash(buffer, size);
+
+    if result & 0xffffff00 == 0 {
+        shared.second.fetch_min(n, Ordering::Relaxed);
+        shared.done.store(true, Ordering::Relaxed);
+    } else if result & 0xfffff000 == 0 {
+        shared.first.fetch_min(n, Ordering::Relaxed);
+    }
+}
+
+fn worker(shared: &Day04) {
+    while !shared.done.load(Ordering::Relaxed) {
+        let offset = shared.counter.fetch_add(1000, Ordering::Relaxed);
+        let (mut buffer, size) = format_string(&shared.prefix, offset);
+
+        for n in 0..1000 {
+            // Format macro is very slow, so update digits directly
+            buffer[size - 3] = b'0' + (n / 100) as u8;
+            buffer[size - 2] = b'0' + ((n / 10) % 10) as u8;
+            buffer[size - 1] = b'0' + (n % 10) as u8;
+
+            check_hash(&mut buffer, size, offset + n, shared);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Day04;
+
+    const INPUT: &'static str = include_str!("input.txt");
+
+    #[test]
+    fn test_part1_examples() {
+        assert_eq!(Day04::part1("abcdef"), 609043);
+        assert_eq!(Day04::part1("pqrstuv"), 1048970);
+    }
+
+    #[test]
+    fn test_part1_puzzle() {
+        assert_eq!(Day04::part1(INPUT), 254575);
+    }
+
+    #[test]
+    fn test_part2_puzzle() {
+        assert_eq!(Day04::part2(INPUT), 1038736);
+    }
+}
