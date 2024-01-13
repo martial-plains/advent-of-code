675622bc36f7214d3234a05041f025334d6cd5b9
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2024-01-13 09:50:45 -0400

    added lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 #![feature(trait_alias)]
-#![warn(clippy::all, clippy::pedantic, missing_debug_implementations)]
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    missing_debug_implementations
+)]
 #![allow(unused)]
 pub mod year_2015;
 
