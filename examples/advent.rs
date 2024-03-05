3489a6777bb7149a978ce355178de67bfddc5b6e
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2024-03-05 16:29:07 -0400

    complete day 10

--- examples/advent.rs
+++ examples/advent.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = advent_of_code::year_2016::day_08::INPUT;
-    let result = advent_of_code::year_2016::day_08::part2(input);
+    let input = advent_of_code::year_2016::day_10::INPUT;
+    let result = advent_of_code::year_2016::day_10::part2(input);
     println!("{result:?}");
 }
