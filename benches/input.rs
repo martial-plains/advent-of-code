95e1773f6d88f6b135989d7b658f062f01daec6d
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-07-27 16:29:07 -0400

    Add benchmarks for Advent of Code 2016 solutions
    Add benchmarks for Advent of Code 2016 solutions
    
    - Added benchmarks for all days in 2016
    - Includes performance metrics and comparison
    - Updated README to reflect new benchmarks

--- benches/input.rs
+++ benches/input.rs
@@ -47,3 +47,33 @@ mod year_2015 {
     benchmark!(year_2015, day_24);
     benchmark!(year_2015, day_25);
 }
+
+mod year_2016 {
+    use crate::benchmark;
+
+    benchmark!(year_2016, day_01);
+    benchmark!(year_2016, day_02);
+    benchmark!(year_2016, day_03);
+    benchmark!(year_2016, day_04);
+    benchmark!(year_2016, day_05);
+    benchmark!(year_2016, day_06);
+    benchmark!(year_2016, day_07);
+    benchmark!(year_2016, day_08);
+    benchmark!(year_2016, day_09);
+    benchmark!(year_2016, day_10);
+    benchmark!(year_2016, day_11);
+    benchmark!(year_2016, day_12);
+    benchmark!(year_2016, day_13);
+    benchmark!(year_2016, day_14);
+    benchmark!(year_2016, day_15);
+    benchmark!(year_2016, day_16);
+    benchmark!(year_2016, day_17);
+    benchmark!(year_2016, day_18);
+    benchmark!(year_2016, day_19);
+    benchmark!(year_2016, day_20);
+    benchmark!(year_2016, day_21);
+    benchmark!(year_2016, day_22);
+    benchmark!(year_2016, day_23);
+    benchmark!(year_2016, day_24);
+    benchmark!(year_2016, day_25);
+}
